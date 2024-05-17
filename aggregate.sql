a6a348ed7c530273827efab9d433e6bc6c953725
Teemu <teemu.kaiturinmaki@gmail.com>
2024-05-17 17:31:50 +0300

    improved sql query for multiple use cases

--- aggregate.sql
+++ aggregate.sql
@@ -1,7 +1,6 @@
-SELECT cc.id, GROUP_CONCAT(attachment_filename SEPARATOR
-';') AS receipts, cc.running_number
-FROM receipts r
-JOIN cost_claims cc ON cc.id = cost_claim_id
-WHERE cost_claim_id IS NOT NULL
+SELECT cc.id, GROUP_CONCAT(attachment_filename SEPARATOR ';') AS receipts, cc.running_number
+FROM receipts r JOIN cost_claims cc ON cc.id = cost_claim_id
+-- WHERE cost_claim_id IS NOT NULL AND MONTH(cc.created) = MONTH(CURDATE())
+WHERE cost_claim_id IS NOT NULL AND MONTH(cc.created) IN (4,5,6)
 GROUP BY cost_claim_id
 ORDER BY cc.running_number;
